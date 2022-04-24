5fd7a94a6c179b093d5a0bb322a0c34f165be93d
Danny Medrano <danny.medrano@ppm.com.ec>
2022-04-24 17:59:33 -0500

    Adding Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import logo from './logo.svg'
-import './css/main.css'
 import { Helmet } from 'react-helmet'
+import Header from './components/Header'
 import Navbar from './components/Navbar'
+import './css/main.css'
 
 function App() {
   return (
@@ -16,13 +16,7 @@ function App() {
       </Helmet>
       <body>
         <Navbar />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <Header />
       </body>
     </div>
   )
--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,1 +1,5 @@
+export { default as popeFrancis2x } from './pope-francis.@2x.png'
+export { default as popeFrancis } from './pope-francis.png'
 export { default as search } from './search.svg'
+export { default as thumbsDown } from './thumbs-down.svg'
+export { default as thumbsUp } from './thumbs-up.svg'
