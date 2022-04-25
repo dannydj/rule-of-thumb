9b37b1ef0eef1b39c5d1448fc2f4f55251e7e053
Danny Medrano <danny.medrano@ppm.com.ec>
2022-04-24 23:44:05 -0500

    Enhancing the Thumb component

--- src/components/Thumb/index.tsx
+++ src/components/Thumb/index.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { thumbsDown, thumbsUp } from '../../assets/img'
 import './styles.css'
 
-export default function Thumb({ className }: { className?: string }): JSX.Element {
-  return <div className={`${className} thumb-container`}>Thumb</div>
+type ComponentProps = {
+  className?: string
+  onClick?: () => void
+  up?: boolean
+}
+export default function Thumb(props: ComponentProps): JSX.Element {
+  const { className, onClick, up } = props
+  const label = `thumbs ${up ? 'up' : 'down'}`
+
+  return (
+    <button className={`${className} thumb-container icon-button`} aria-label={label} onClick={onClick ? () => onClick() : undefined}>
+      <img src={up ? thumbsUp : thumbsDown} alt={label} className="thumb" />
+    </button>
+  )
 }
